3f934e50529b43cefef13d326d785b1e96e38d99
arthur12087 <arthurferare@gmail.com>
2024-04-16 07:46:39 -0300

    finalizando exercicio

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -22,19 +22,17 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //1 selecionar produtos ao carrinho, 2- preencher opções no checkout, 3 -validar compra
 
         for(let i =0; i<dadosProdutos.length; i++){
 
         productPage.searchProduct(dadosProdutos[i].nomeproduto)
         productPage.addProductToCart(dadosProdutos[i].nomeproduto, dadosProdutos[i].tamanho, dadosProdutos[i].cor, dadosProdutos[i].quantidade)
         }
-
-    });
-
-    it.only('Preencher checkout após colocar produtos no carrinho', () => {
         checkoutPage.preencherCheckout()
+
     });
+
+   
 
 
 })
